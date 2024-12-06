eb13180321016774d0b6c46f2217bb3576e767d8
fuguigui <fgr079@126.com>
2024-12-06 18:00:32 +0100

    change day5 content

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,38 +1,3 @@
-# the item looks like a number to a set, e.g. 1: {2,3,5}
-# The value set includes all the numbers smaller than the key based on the given order definition.
-
-def check_order(input_list, num_to_smaller_map):
-    if len(input_list) <= 1:
-        return True
-    first_item, second_item = input_list[0], input_list[1]
-    if second_item in num_to_smaller_map[first_item]:
-        return False
-    return check_order(input_list[1:], num_to_smaller_map)
-
-def test_check_order():
-    num_to_smaller_map = {
-        13: {29, 53, 61, 47, 75, 97}, 
-        29: {53, 61, 47, 75, 97},
-        53: {61, 47, 75, 97},
-        61: {47, 75, 97},
-        47: {75, 97},
-        75: {97},
-        97: {}}
-    input_lists = [
-        [75,47,61,53,29],
-        [97,61,53,29,13],
-        [75,29,13],
-        [75,97,47,61,53],
-        [61,13,29],
-        [97,13,75,29,47]
-    ]
-    expected_output = [True, True, True, False, False, False]
-    for i, input_list in enumerate(input_lists):
-        print("actual output: {}".format(check_order(input_list, num_to_smaller_map)))
-        print("expected output: {}".format(expected_output[i]))
-
-# test_check_order()
-
 def parse_pair_list(inputs):
     pair_list = []
     for line in inputs:
@@ -76,20 +41,15 @@ def test_read_input():
 
 # test_read_input()
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.children = []
+def check_order(input_list, num_to_bigger_map):
+    if len(input_list) <= 1:
+        return True
+    first_item, second_item = input_list[0], input_list[1]
+    # print("check_order  for the list: {}".format(input_list[:2]))
+    if first_item not in num_to_bigger_map or second_item not in num_to_bigger_map[first_item]:
+        return False
+    return check_order(input_list[1:], num_to_bigger_map)
 
-    def get_all_children_value(self):
-        return [child.data for child in self.children]
-    
-    def __str__(self):
-        return f"Node({self.data}, childrens: {self.get_all_children_value()})" 
-
-def print_num_to_node_map(num_to_node_map):
-    for node in num_to_node_map.values():
-        print(node)
 
 def get_or_insert(dictionary, key, default_value):
     value = dictionary.get(key)
@@ -98,91 +58,81 @@ def get_or_insert(dictionary, key, default_value):
         return default_value
     return value
 
-def build_smaller_trees(pair_list):
-    number_to_node_map = {}
-    roots = set()
-    for pair in pair_list:
-        smaller_num, greater_num = pair[0], pair[1]
-        # print("smaller_num: {}, greater_num: {}, roots: {}".format(smaller_num, greater_num, roots))
-        if greater_num not in roots and greater_num not in number_to_node_map:
-            roots.add(greater_num)
-        if smaller_num in roots:
-            roots.remove(smaller_num)
-        print("roots: {}".format(roots))
-        smaller_node = get_or_insert(number_to_node_map, smaller_num, Node(smaller_num))
-        greater_node = get_or_insert(number_to_node_map, greater_num, Node(greater_num))
-        greater_node.children.append(smaller_node)
-    return roots, number_to_node_map
-
-def test_build_smaller_trees():
-    parsed_rules, _ = read_input("sample.txt")
-    roots, num_to_node_map = build_smaller_trees(parsed_rules)
-    print(roots)
-    print_num_to_node_map(num_to_node_map)
-
-# test_build_smaller_trees()
-
-num_to_smaller_map = {} # This is a global variable.
-
-def traverse_tree(root, number_to_node_map):
-    if len(number_to_node_map[root].children) == 0:
-        num_to_smaller_map[root] = {}
-        # print(num_to_smaller_map)
-        return
-    children_set = set()
-    # print("traverse for the root: {}, with childer: {}".format(root, number_to_node_map[root].get_all_children_value()))
-    for child in number_to_node_map[root].children:
-        # print("traverse for the child: {}".format(child.data))
-        if child.data not in num_to_smaller_map:
-            traverse_tree(child.data, number_to_node_map)
-        children_set.add(child.data)
-        children_set.union(num_to_smaller_map[child.data])
-    num_to_smaller_map[root] = children_set
-    # print("after traverse, num_to_smaller_map {}".format(num_to_smaller_map))
-    return
-
-
-def traverse_trees(roots, number_to_node_map):
-    for root in roots:
-        traverse_tree(root, number_to_node_map)
-
-def build_smaller_map(pair_list):
-    roots, num_to_node_map = build_smaller_trees(pair_list)
-    traverse_trees(roots, num_to_node_map)
-    return
-
-def test_build_smaller_map():
-    parsed_rules, _ = read_input("input.txt")
-    build_smaller_map(parsed_rules)
-    print(num_to_smaller_map)
-
-# 67 | 98, 98 | 87, 87 | 67. There is a circle in the input.
-test_build_smaller_map()
+def build_num_to_bigger_map(pair_lists):
+    num_to_bigger_map = {}
+    for pair in pair_lists:
+        bigger_set = get_or_insert(num_to_bigger_map, pair[0], set())
+        if pair[1] not in bigger_set:
+            bigger_set.add(pair[1])
+    return num_to_bigger_map
 
 def get_middle_number(input_list):
     assert(len(input_list) % 2 == 1)
     return input_list[int((len(input_list) - 1 )/2)]
 
-def test_sample():
-    parsed_rules, parsed_lists = read_input("sample.txt")
-    build_smaller_map(parsed_rules)
+def get_middle_sums(file_name):
+    parsed_rules, parsed_lists = read_input(file_name)
+    num_to_bigger_map = build_num_to_bigger_map(parsed_rules)
+    # print(num_to_bigger_map)
     middle_sum = 0
     for parsed_list in parsed_lists:
-        if check_order(parsed_list, num_to_smaller_map):
+        if check_order(parsed_list, num_to_bigger_map):
             middle_sum += get_middle_number(parsed_list)
-    print("actual output: {}".format(middle_sum))
-    print("expected output: {}".format(143))
+    return middle_sum
 
+
+def test_sample():
+    print("actual output: {}".format(get_middle_sums("sample.txt")))
+    print("expected output: {}".format(143))
 
 # test_sample()
 
-def main_part1():
-    parsed_rules, parsed_lists = read_input("input.txt")
-    build_smaller_map(parsed_rules)
+######### Part1 #########
+# print(get_middle_sums("input.txt"))
+
+
+######### Part2 #########
+
+def filter_bigger_map(input_list, num_to_bigger_map):
+    in_list_map = {}
+    for item in input_list:
+        in_list_map[item] = num_to_bigger_map[item]
+    key_sets = set(in_list_map.keys())
+    for item, value in in_list_map.items():
+        in_list_map[item] = key_sets.intersection(value)
+    return in_list_map
+
+def get_middle_number_reordered(input_list, bigger_map, cur_index, aim_index):
+    biggest_num = -1
+    for key, values in bigger_map.items():
+        if len(values) == 0:
+            biggest_num = key
+            break
+    assert(biggest_num != -1)
+    if cur_index == aim_index:
+        return biggest_num
+    new_bigger_map = {}
+    new_input_list = []
+    for input in input_list:
+        if input == biggest_num:
+            continue
+        new_input_list.append(input)
+        old_values = bigger_map[input]
+        if biggest_num in old_values:
+            old_values.remove(biggest_num)
+        new_bigger_map[input] = old_values
+    return get_middle_number_reordered(new_input_list, new_bigger_map, cur_index + 1, aim_index)
+            
+
+def get_middle_sums_reordered(file_name):
+    parsed_rules, parsed_lists = read_input(file_name)
+    num_to_bigger_map = build_num_to_bigger_map(parsed_rules)
+    # print(num_to_bigger_map)
     middle_sum = 0
     for parsed_list in parsed_lists:
-        if check_order(parsed_list, num_to_smaller_map):
-            middle_sum += get_middle_number(parsed_list)
-    print(middle_sum)
+        if not check_order(parsed_list, num_to_bigger_map):
+            filtered_bigger_map = filter_bigger_map(parsed_list, num_to_bigger_map)
+            middle_sum += get_middle_number_reordered(parsed_list, filtered_bigger_map, 0, int((len(parsed_list) - 1) / 2))
+    return middle_sum
 
-# main_part1()
+print(get_middle_sums_reordered("input.txt"))
